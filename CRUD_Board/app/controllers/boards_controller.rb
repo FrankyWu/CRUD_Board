0aeac84b6ec489cf581aac0dbab10306d5c88551
FrankyWu <forefeel741114@gmail.com>
2013-10-07 17:51:18 +0800

    first commit

---
+++ CRUD_Board/app/controllers/boards_controller.rb
@@ -0,0 +1,51 @@
+class BoardsController < ApplicationController
+  # GET /boards
+  # GET /boards.json
+  def index
+	@boards = Board.all
+
+	respond_to do |format|
+	  format.html # index.html.erb
+	  format.json { render json: @boards}
+	end
+  end
+
+  # GET /boards/1
+  # GET /boards/1.json
+  def show
+    @board = Board.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @board }
+    end
+  end
+
+  # GET /boards/new
+  # GET /boards/new.json
+  def new
+    @board = Board.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @board }
+    end
+  end
+
+  # POST /boards
+  # POST /boards.json
+  def create
+    @board = Board.new(params[:board])
+
+    respond_to do |format|
+      if @board.save
+        format.html { redirect_to @board, notice: 'Board was successfully created.' }
+        format.json { render json: @board, status: :created, location: @board }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @board.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end
---
+++ CRUD_Board/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRUDBoard::Application.config.secret_token = 'c0b7077dfe70fb46c6ade3195dab94f07b03dd4f23fa3449fcdd8d84bda79d5e46242933f6911e360a3194ce61e870f17c07334f64ebb887c170af4d4547744a'
