de4037c2ab230f06210903b71ab1c4781176f2de
FrankyWu <forefeel741114@gmail.com>
2013-10-08 11:29:51 +0800

    Board Finish

--- CRUD_Board/app/controllers/boards_controller.rb
+++ CRUD_Board/app/controllers/boards_controller.rb
@@ -48,4 +48,38 @@ class BoardsController < ApplicationController
     end
   end
 
+  # GET /boards/1/edit
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  # PUT /boards/1
+  # PUT /boards/1.json
+  def update
+    @board = Board.find(params[:id])
+
+    respond_to do |format|
+      if @board.update_attributes(params[:board])
+        format.html { redirect_to @board, notice: 'Board was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @board.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /boards/1
+  # DELETE /boards/1.json
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+
+    respond_to do |format|
+      format.html { redirect_to boards_url }
+      format.json { head :no_content }
+    end
+  end
+
+
 end
